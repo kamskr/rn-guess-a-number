61ee0fca4b6ab25f5b4fc8c6fdd4d6d2bcc0efa9
Kamil <sikorakamil26@gmail.com>
2020-11-01 21:52:38 +0100

    added alert

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -6,6 +6,7 @@ import {
   Button,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from "react-native";
 import Card from "../components/Card";
 import { theme } from "../constants/theme";
@@ -29,7 +30,12 @@ const StartGameScreen: React.FC<StartGameScreenProps> = (props) => {
 
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
-    if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber >= 99) {
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber >= 99) {
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 1 and 99.",
+        [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
+      );
       return;
     }
     setConfirmed(true);
