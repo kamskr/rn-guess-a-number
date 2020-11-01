b13485894d9600d3f7730c832b11346167fafd36
Kamil <sikorakamil26@gmail.com>
2020-11-01 20:25:31 +0100

    add simple card

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 interface StartGameScreenProps {}
 
 const StartGameScreen: React.FC<StartGameScreenProps> = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>Some Text</Text>
+      <Text style={styles.title}>Start a New Game</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -15,6 +23,28 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: "center",
+  },
+  title: { fontSize: 20, marginVertical: 10 },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    // For IOS
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.16,
+    // For Android
+    elevation: 5,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
   },
 });
 
